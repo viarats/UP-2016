9d5e019cad79ae1fd204a3bdba8ab072fe1afcc8
viarats <vt7991@gmail.com>
2016-11-01 20:43:16 +0200

    Add homework1, task1

---
+++ Homework/hw1/task1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num1, num2, num3, num4, num5, max;
+  cout << "First number = ";
+  cin >> num1;
+  cout << "Second number = ";
+  cin >> num2;
+  cout << "Third number = ";
+  cin >> num3;
+  cout << "Fourth number = ";
+  cin >> num4;
+  cout << "Fifth number = ";
+  cin >> num5;
+  max = num1;
+
+  if (max < num2) {
+  	 max = num2;
+  }
+  if (max < num3) {
+  	 max = num3;
+  }
+  if (max < num4) {
+  	 max = num4;
+  }
+  if (max < num5) {
+  	 max = num5;
+  }
+
+  cout << max << endl;
+
+  return 0;
+}
